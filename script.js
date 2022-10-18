bb4ede3e47f778b50f819df798a5ddd41d2e6eb6
AliAliAliAli <alihasan404439@gmail.com>
2022-10-18 15:14:19 +0600

    add to git file welcome bangladesh

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var fristname = "welcome "
 var lestname = "  bangladesh  "
 var fullname = fristname.trim() + " " + lestname.trim()
+fullname = fullname[0].toUpperCase() + fullname.slice(1)
+var index = fullname.indexOf(" ")
+fullname = fullname.slice(0, index + 1) + fullname[index + 1].toUpperCase() + fullname.slice(index + 2)
 console.log(fullname)
